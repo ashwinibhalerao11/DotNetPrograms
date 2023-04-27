0cec3294d9fe83a13ed20846bdf4606a414637a8
ashwinibhalerao11 <bhaleraoashwini215@gmail.com>
2023-04-27 13:08:08 +0530

    Add project files.

---
+++ DotNetPrograms/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetPrograms
+{
+    //declaration of delegate
+    public delegate int MyDelegate(int a, int b);
+    public class Calc
+    {
+       
+        public int Add(int a, int b)
+        { 
+            return a + b;   
+        }
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+    public  class Program
+    {
+        static void Main(string[] args)
+        {
+            Calc calc=new Calc();
+            MyDelegate del=new MyDelegate(calc.Sub);//add method reference
+            del += new MyDelegate(calc.Add);
+            del += new MyDelegate(calc.Multiply);
+
+            // invcation list
+            Delegate[] list=del.GetInvocationList();
+            foreach (Delegate item in list) 
+            {
+                Console.WriteLine(item.Method);
+            }
+        }
+    }
+}
